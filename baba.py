3f70dcaf65559fccd7a3e500cb9acc78d482a3fd
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-24 13:39:34 +0600

    Create baba.py

---
+++ baba.py
@@ -0,0 +1,10 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+os.system("git clone https://github.com/Shuvo-BBHH/paidfree4 && cd paidfree4 && python mahdi.py")
